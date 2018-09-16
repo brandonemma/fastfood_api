a18da164f74782001ff9c310d0306a1fb7920ef3
masaba emma <brandonemma13@gmail.com>
2018-09-16 11:01:13 +0300

    Revert lost changes lost the persistent order object in orders data structures so i updated with the working version on the remote repository
    [finishes #160533595][finishes #160533578][finishes #160533569][finishes #160533548]

--- routes.py
+++ routes.py
@@ -3,12 +3,19 @@ from flask_restful import reqparse, abort, Api, Resource
 import json
 import pickle
 
-
 app = Flask(__name__)
 api = Api(app)
 
 #orders dictionary memory storage
-orders = {}
+orders = {
+    "1":
+   {'orderId':1,
+    'name':'oranges',
+    'price':500,
+    'picture':'oranges.jpg',
+    'status':'new order'} 
+}
+
 
 def abort_if_order_doesnt_exist(order_id):
     if order_id not in orders:
